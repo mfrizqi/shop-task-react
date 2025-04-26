89defb965200263216fd500535ebf724fa64ee0e
mfrizqi <muhammadfajarrizqi@gmail.com>
2025-04-26 13:03:51 +0800

    fix: Remove unused import Events

--- src/page/TaskManager.tsx
+++ src/page/TaskManager.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, MouseEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import TaskCard from "../components/TaskCard";
 import { v4 as uuidv4 } from 'uuid';
 
